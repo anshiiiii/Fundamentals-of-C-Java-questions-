772058fc1ebc2601c1891ea4e5af28d0784f04c3
anshiiiii <vsatyana@gitam.in>
2024-02-28 00:16:44 +0530

    update day4

---
+++ C_Training/Daily_Test/DAY4_1.c
@@ -0,0 +1,27 @@
+//tranpose
+#include <stdio.h>
+int main()
+{
+  int m,n,a[100][100];
+  scanf("%d %d",&m,&n);
+  for(int i = 0;i < m;i++)
+  {
+    for(int j = 0;j < n;j++)
+    {
+       scanf("%d",&a[i][j]);
+       printf("%d ",a[i][j]);
+    }
+    printf("\n");
+  }
+  printf("Transpose matrix is:\n");
+  for(int i = 0;i < n;i++)
+  {
+    for(int j = 0;j < m;j++)
+    {
+       printf("%d ",a[j][i]);
+    }
+    printf("\n");
+  }
+  
+   return 0;
+}
