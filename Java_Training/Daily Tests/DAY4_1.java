5dc3dacbf0342dd880f34a26ca1f4eb41d926133
anshiiiii <vsatyana@gitam.in>
2024-03-05 21:49:09 +0530

    day4test

---
+++ Java_Training/Daily Tests/DAY4_1.java
@@ -0,0 +1,53 @@
+/*Write a Java Program to print addition of N numbers.
+
+
+
+Given N integer numbers (user will read numbers one by one) and find sum/addition of all given numbers.
+
+
+
+Input format :
+
+Input consist of an Integer
+
+Output format:
+
+Output consist of an Integer
+
+
+
+Sample Input :
+
+5
+
+100
+
+200
+
+300
+
+400
+
+500
+
+Sample Output :
+
+1500 */
+import java.util.Scanner;
+
+public class DAY4_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+        
+        int sum = 0;
+        
+        for (int i = 0; i < n; i++) {
+            int num = scanner.nextInt();
+            sum += num;
+        }
+        
+        System.out.println("" + sum);
+    }
+}
---
+++ Java_Training/Daily Tests/DAY4_2.java
@@ -0,0 +1,37 @@
+/*Write a Java Program to Print a Diamond Shape Pattern.
+
+
+
+Input format:
+
+Input consist of an Integer.
+
+Output format:
+
+Output consist of a
+
+
+
+Sample Input :
+
+5
+
+Sample Output :
+
+ *\n  ***\n *****\n *******\n*********\n *******\n *****\n  ***\n  * */
+import java.util.Scanner;
+
+public class DAY4_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+        
+        for (int i = 1; i <= n; i++) {
+            System.out.println(" ".repeat(n - i) + "*".repeat(2 * i - 1));
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            System.out.println(" ".repeat(n - i) + "*".repeat(2 * i - 1));
+        }
+    }
+}
